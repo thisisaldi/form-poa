bff93245d1706c30175be9ffa3f2bbd922ed82dd
Bryan Fernando <bryanfernandoks11@gmail.com>
2026-07-14 11:18:04 +0700

    chore: add migration script

---
+++ scripts/migrate.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
+
+mv /vault/secrets/.env.staging $WORKDIR/staging/.env.staging
+mv /vault/secrets/.env.production $WORKDIR/production/.env.production
+
+npx prisma migrate deploy
