5ff1c74a355d6ca05687c3086ef2a99e8956cfbe
Bryan Fernando <bryanfernandoks11@gmail.com>
2026-07-14 17:22:04 +0700

    ops: apply env variable from .env file

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -3,7 +3,8 @@
 WORKDIR=/app
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 
-cp /vault/secrets/.env.${NAMESPACE} $WORKDIR/.env.${NAMESPACE}
+cp /vault/secrets/.env.$NAMESPACE $WORKDIR/.env.$NAMESPACE
+export $(grep -v '^#' $WORKDIR/.env.$NAMESPACE | xargs)
 
 npx prisma generate
 npm run db:migrate
