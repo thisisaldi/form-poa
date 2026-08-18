b8759b3305ae5805126414d35055cd74c6965858
Bryan Fernando <bryanfernandoks11@gmail.com>
2026-08-18 11:26:24 +0700

    ops: update migrate script

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -4,7 +4,9 @@ WORKDIR=/app
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 
 cp /vault/secrets/.env.$NAMESPACE $WORKDIR/.env.$NAMESPACE
-export $(grep -v '^#' $WORKDIR/.env.$NAMESPACE | xargs)
+set -a
+source $WORKDIR/.env.$NAMESPACE
+set +a
 
 npx prisma generate
 npm run db:migrate
